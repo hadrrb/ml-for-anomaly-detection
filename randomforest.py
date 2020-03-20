2e63d3f555a25c8667c58302f2946c6635b3761b
Ramzi Hadrich <hadrichb@gmail.com>
2020-03-21 00:28:31 +0100

    rainforest ok

--- randomforest.py
+++ randomforest.py
@@ -1,2 +1,26 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
+from sklearn import preprocessing
+import pandas as pd
+import numpy as np
+
+X = pd.read_csv("Data/data1.csv")
+
+le = preprocessing.LabelEncoder()
+le.fit(["NoEvents", "Attack", "Natural"])
+
+y = le.transform(X['marker'])
+X = X.drop(columns='marker')
+
+X = X.replace(np.inf, np.finfo(np.float32).max)
+
+clf = RandomForestClassifier()
+clf.fit(X, y)
+
+X1 = pd.read_csv("Data/data14.csv")
+
+y1 = le.transform(X1['marker'])
+X1 = X1.drop(columns='marker')
+X1 = X1.replace(np.inf, np.finfo(np.float32).max)
+
+y1p = clf.predict(X1)
+print(np.sum(y1 == y1p)/y1.size)
